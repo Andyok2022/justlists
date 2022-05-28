f5b97dc0ae7bd284007f5968b8dee2ca2948ed73
Alston <Alston106@gmail.com>
2022-05-28 14:25:13 +0400

    add my token

---
+++ token.ts
@@ -0,0 +1,19 @@
+{
+ "name": "AJ.VIP", #List name
+ "logoURI": "https://aj.vip/ajviplogo.png", #List logo uri
+ "timestamp": 1653725602000, #Update time in millisecond
+ "tokens": [{
+ "chainId": 1, #ChainId, which is currently 1
+ "address": "TNTsQstXg6JyxZyNTJedJ1SbCirc1v9bz2", #Token address
+ "name": "AJ.VIP Hash", #Token name
+ "symbol": "AJ.VIP", #Token symbol
+ "decimals": 6, #Token decimals
+ "logoURI": "https://aj.vip/ajviplogo.png" #Token logo uri
+ }
+ ],
+ "version": { #Version info
+ "major": 1, #Major version
+ "minor": 0, #Minor version
+ "patch": 0 #Patch version
+ }
+}
